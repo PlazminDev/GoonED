d09db0eeabe650191e39be46e40e368f1d00f52f
Plazmin <endermanrapman@gmail.com>
2024-06-25 09:28:06 -0400

    Move To C#

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using ImGuiNET;
+using System.Diagnostics;
+using System.Numerics;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+using static ImGuiNET.ImGuiNative;
+
+namespace GoonED
+{
+    class Program
+    {
+        private static Sdl2Window _window;
+        private static GraphicsDevice _gd;
+        private static CommandList _cl;
+        private static ImGuiController _controller;
+
+        private static void Main(string[] args)
+        {
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                new WindowCreateInfo(50, 50, 1280, 720, WindowState.Normal, "GoonED"),
+                new GraphicsDeviceOptions(true, null, true, ResourceBindingModel.Improved, true, true),
+                out _window,
+                out _gd
+            );
+
+            _window.Resized += () =>
+            {
+                _gd.MainSwapchain.Resize((uint)_window.Width, (uint)_window.Height);
+                _controller.WindowResized(_window.Width, _window.Height);
+            };
+            _cl = _gd.ResourceFactory.CreateCommandList();
+            _controller = new ImGuiController(_gd, _gd.MainSwapchain.Framebuffer.OutputDescription, _window.Width, _window.Height);
+
+            var stopwatch = Stopwatch.StartNew();
+            float deltaTime = 0f;
+            while (true)
+            {
+                deltaTime = stopwatch.ElapsedTicks / (float)Stopwatch.Frequency;
+                stopwatch.Restart();
+                InputSnapshot snapshot = _window.PumpEvents();
+                if (!_window.Exists) break;
+
+                _controller.Update(deltaTime, snapshot);
+
+                DrawGUI();
+
+                _cl.Begin();
+                _cl.SetFramebuffer(_gd.MainSwapchain.Framebuffer);
+                _cl.ClearColorTarget(0, new RgbaFloat(0f, 0f, 0f, 1f));
+                _controller.Render(_gd, _cl);
+                _cl.End();
+                _gd.SubmitCommands(_cl);
+                _gd.SwapBuffers(_gd.MainSwapchain);
+            }
+
+            // Clean up Veldrid resources
+            _gd.WaitForIdle();
+            _controller.Dispose();
+            _cl.Dispose();
+            _gd.Dispose();
+        }
+
+        private static unsafe void DrawGUI()
+        {
+            ImGui.Begin("Editor");
+
+            ImGui.Text("Hello, world!");
+
+            ImGui.End();
+        }
+    }
+}
